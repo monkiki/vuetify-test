87dc2575d9a2460ca4d2692c34f28d8b114b0aad
Paco Avila <pavila@openkm.com>
2019-05-16 14:10:21 +0200

    Vuetify integration

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app dark color="blue">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat href="#" target="_blank">
+        <span class="mr-2">Hello</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+    <v-footer dark color="blue">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  import HelloWorld from './components/HelloWorld'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
